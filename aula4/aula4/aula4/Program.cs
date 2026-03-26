c2bdbebee2632cf9a80a57cf41b9d73b363055c9
Heverton R.S Santos <hsuchek47@gmail.com>
2026-03-26 09:41:14 -0300

    Add files via upload

---
+++ aula4/aula4/aula4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program{
+    
+class Produto{
+
+    public string nome;
+    public double preco;
+
+    public void exibir(){
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Preco:" + preco);
+    }
+}
+static void Main(){
+
+    Console.WriteLine("Hello, World!");
+    }
+}
